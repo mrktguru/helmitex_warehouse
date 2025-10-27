c9628150f5af9849ec4d5bcd27fc389302233c17
mrktguru <gommeux@gmail.com>
2025-10-27 23:04:29 +0200

    Update __init__.py

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -2,4 +2,9 @@
 Бизнес-логика приложения.
 """
 
-# Импорты будут добавлены после разделения services.py
+# Временно импортируем из _original_services
+# После разделения на модули это будет обновлено
+
+from . import _original_services
+
+__all__ = ["_original_services"]
