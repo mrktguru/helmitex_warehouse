d4d20ba9185daf5ec3f6b22f4678c8e48a2b8187
mrktguru <gommeux@gmail.com>
2025-10-27 23:56:17 +0200

    Update __init__.py

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,25 +1,50 @@
 """
-Вспомогательные функции.
+Вспомогательные функции и утилиты.
 """
 
 from .helpers import (
     parse_key_value_lines,
-    format_sku_list,
-    format_recipe_list,
-    format_category_list,
-    format_barrel_list,
-    format_weight,
-    format_percentage,
     validate_percentage_sum
 )
 
+from .formatters import (
+    format_category_list,
+    format_raw_material_list,
+    format_semi_product_list,
+    format_finished_product_list,
+    format_recipe_list,
+    format_recipe_details,
+    format_materials_check,
+    format_movement_history,
+    format_date,
+    format_weight,
+    format_percentage
+)
+
+from .decorators import (
+    admin_only,
+    with_db_session
+)
+
 __all__ = [
+    # helpers
     "parse_key_value_lines",
-    "format_sku_list",
-    "format_recipe_list",
+    "validate_percentage_sum",
+    
+    # formatters
     "format_category_list",
-    "format_barrel_list",
+    "format_raw_material_list",
+    "format_semi_product_list",
+    "format_finished_product_list",
+    "format_recipe_list",
+    "format_recipe_details",
+    "format_materials_check",
+    "format_movement_history",
+    "format_date",
     "format_weight",
     "format_percentage",
-    "validate_percentage_sum",
+    
+    # decorators
+    "admin_only",
+    "with_db_session",
 ]
