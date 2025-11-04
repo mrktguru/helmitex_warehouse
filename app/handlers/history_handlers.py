7fed7b63ab8e0743ae22bec6f8cdc3ccf550a906
mrktguru <gommeux@gmail.com>
2025-11-04 23:57:02 +0200

    Create history_handlers.py

---
+++ app/handlers/history_handlers.py
@@ -0,0 +1,12 @@
+"""
+Обработчики истории операций.
+
+Функции:
+- history_menu_callback()
+- history_arrival_callback()
+- history_production_callback()
+- history_packing_callback()
+- history_shipment_callback()
+- history_all_callback()
+- history_detail_callback() - детали операции
+"""
