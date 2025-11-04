5a7a6a0195780cc3292a54d1e5a2a75531fb36c0
mrktguru <gommeux@gmail.com>
2025-11-04 23:56:46 +0200

    Create stock_handlers.py

---
+++ app/handlers/stock_handlers.py
@@ -0,0 +1,11 @@
+"""
+Обработчики просмотра остатков.
+
+Функции:
+- stock_menu_callback()
+- stock_raw_callback() - остатки сырья
+- stock_semi_callback() - остатки полуфабрикатов (бочек)
+- stock_finished_callback() - остатки готовой продукции
+- stock_by_category_callback() - фильтр по категории
+- stock_low_callback() - товары с низким остатком
+"""
